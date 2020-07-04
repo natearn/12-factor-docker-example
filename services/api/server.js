9c0b67e43f7250047e59b04d06717bde70ad2a7a
Nathan Arnold <natearn@natearn.info>
2020-07-04 11:03:14 -0400

    Create a simple express API for the website to use

---
+++ services/api/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+const port = 4000;
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/categories', (req, res) => {
+  const categories = [
+    'beaches',
+    'birds'
+    'food',
+    'mountain',
+  ];
+  return res.status(200).json(categories);
+});
+
+app.listen(port, () => console.log(`SnapShot API listening on port ${port}!`));
